56ae8183a3b6aff4cfbd49a1d1f050ac71373115
eric <e.salberg213@gmail.com>
2025-04-11 22:48:45 -0400

    remapped neogit command

--- lua/king/remap.lua
+++ lua/king/remap.lua
@@ -5,7 +5,23 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+vim.keymap.set("n", "<leader>ng", function()
+	vim.cmd("Neogit")
+end)
+
+vim.keymap.set("n", "<leader>b", function()
+	vim.cmd("split")
+end)
+
+vim.keymap.set("n", "<leader>v", function()
+	vim.cmd("vsplit")
+end)
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
